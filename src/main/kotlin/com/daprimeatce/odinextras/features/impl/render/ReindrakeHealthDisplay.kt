65373f24342d19cfcc22103eafbe6bbc5efcc48e
TurtleGD <113803739+TurtleGD@users.noreply.github.com>
2026-07-01 18:54:29 -0400

    Added reindrake health display

---
+++ src/main/kotlin/com/daprimeatce/odinextras/features/impl/render/ReindrakeHealthDisplay.kt
@@ -0,0 +1,42 @@
+package com.daprimeatce.odinextras.features.impl.render
+
+import com.odtheking.odin.clickgui.settings.impl.NumberSetting
+import com.odtheking.odin.events.RenderEvent
+import com.odtheking.odin.events.core.on
+import com.odtheking.odin.features.Module
+import com.odtheking.odin.utils.render.drawText
+import net.minecraft.world.phys.Vec3
+import net.minecraft.world.entity.boss.enderdragon.EnderDragon
+import com.odtheking.odin.utils.handlers.TickTask
+import com.odtheking.odin.utils.renderX
+import com.odtheking.odin.utils.renderY
+import com.odtheking.odin.utils.renderZ
+import com.odtheking.odin.utils.skyblock.Island
+import com.odtheking.odin.utils.skyblock.LocationUtils
+import net.minecraft.world.entity.Entity
+
+
+object ReindrakeHealthDisplay : Module(
+    name = "Reindrake Health Display",
+    description = "Displays the health of Reindrakes."
+) {
+    var dragons = listOf<Entity>()
+    private val size by NumberSetting("Example Number Setting", 1f, 0.5f, 2f, 0.1f, desc = "Scale of health display.")
+
+    init {
+        TickTask(20) { // Scans for entities every 20t
+            if (LocationUtils.currentArea != Island.JerryWorkshop) return@TickTask
+            val entities = mc.level?.entitiesForRendering() ?: return@TickTask
+
+            dragons = entities.filterIsInstance<EnderDragon>()
+        }
+
+        on<RenderEvent.Extract> {
+            dragons.forEach { dragon ->
+                val dragonEntity = dragon.asLivingEntity()
+                if (dragonEntity != null) drawText("§a${dragonEntity.getHealth().toInt()}§c❤",
+                    Vec3(dragonEntity.renderX, dragonEntity.renderY, dragonEntity.renderZ), size * 7, false)
+            }
+        }
+    }
+}
