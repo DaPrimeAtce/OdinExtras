cf0d0f969881639a4ede3ba9fd57a5b8e5422544
TurtleGD <113803739+TurtleGD@users.noreply.github.com>
2026-07-01 18:24:49 -0400

    Finished arrow poison display

--- src/main/kotlin/com/daprimeatce/odinextras/features/impl/skyblock/ArrowPoisonDisplay.kt
+++ src/main/kotlin/com/daprimeatce/odinextras/features/impl/skyblock/ArrowPoisonDisplay.kt
@@ -1,33 +1,49 @@
 package com.daprimeatce.odinextras.features.impl.skyblock
 
-import com.odtheking.odin.clickgui.settings.Setting.Companion.withDependency
-import com.odtheking.odin.clickgui.settings.impl.BooleanSetting
-import com.odtheking.odin.clickgui.settings.impl.ColorSetting
-import com.odtheking.odin.clickgui.settings.impl.SelectorSetting
-import com.odtheking.odin.events.ChatPacketEvent
-import com.odtheking.odin.events.GuiEvent
 import com.odtheking.odin.events.TickEvent
-import com.odtheking.odin.events.LevelEvent
 import com.odtheking.odin.events.core.on
 import com.odtheking.odin.features.Module
+import com.odtheking.odin.utils.itemId
 import com.odtheking.odin.utils.render.ItemStateRenderer.Companion.drawItemStack
 import com.odtheking.odin.utils.render.textDim
-import com.odtheking.odin.utils.skyblock.dungeon.DungeonUtils
-import net.minecraft.client.gui.GuiGraphicsExtractor
-import net.minecraft.world.entity.EquipmentSlot
-import net.minecraft.world.item.DyeColor
-import net.minecraft.world.item.DyeItem
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.Items
-import net.minecraft.world.level.ItemLike
+
 
 object ArrowPoisonDisplay : Module(
     name = "Arrow Poison Display",
     description = "Shows the number of toxic and twilight arrow poisons in your inventory."
 ) {
+    private var numToxic = 0
+    private var numTwilight = 0
+
     private val hud by HUD(name, "Shows the number of arrow poisons in the HUD.") {
-        var width = textDim("Test", 20, 0).first
-        drawItemStack(ItemStack(Items.LIME_DYE), 0, -5)
-        width to mc.font.lineHeight * 2
+        var widthToxic = textDim("${color(numToxic)}${numToxic}x", 20, 0).first
+        var widthTwilight = textDim("${color(numTwilight)}${numTwilight}x", 20, mc.font.lineHeight + 5).first
+
+        drawItemStack(ItemStack(Items.LIME_DYE), 0, -4)
+        drawItemStack(ItemStack(Items.PURPLE_DYE), 0, mc.font.lineHeight)
+
+        widthTwilight.coerceAtMost(widthToxic) + 20 to mc.font.lineHeight * 2 + 5
+    }
+
+    init {
+        on<TickEvent.End> {
+            numToxic = 0
+            numTwilight = 0
+            val items = mc.player?.inventory?.getNonEquipmentItems() ?: return@on
+
+            for (stack in items) {
+                if (!stack.isEmpty() && stack.itemId == "TOXIC_ARROW_POISON") {
+                    numToxic += stack.getCount()
+                } else if (!stack.isEmpty() && stack.itemId == "TWILIGHT_ARROW_POISON") {
+                    numTwilight += stack.getCount()
+                }
+            }
+        }
+    }
+
+    private fun color(num: Int): String {
+        return if (num > 0) "§a" else "§c"
     }
 }
