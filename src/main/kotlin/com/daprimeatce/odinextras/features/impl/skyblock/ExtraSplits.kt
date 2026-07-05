34b18291f8e6e4aa7a5e0a4e5edd5c15d01876cc
DaPrimeAtce <daprimeatce@gmail.com>
2026-07-04 23:00:41 -0600

    added a somewhat functional Time Lost to Lag splits timer
    - Send Time Lost doesn't work yet
    - this can be optimized/made better to look more readable when in-game

--- src/main/kotlin/com/daprimeatce/odinextras/features/impl/skyblock/ExtraSplits.kt
+++ src/main/kotlin/com/daprimeatce/odinextras/features/impl/skyblock/ExtraSplits.kt
@@ -1,10 +1,13 @@
 package com.daprimeatce.odinextras.features.impl.skyblock
 
+import com.odtheking.odin.clickgui.settings.Setting.Companion.withDependency
 import com.odtheking.odin.clickgui.settings.impl.BooleanSetting
+import com.odtheking.odin.clickgui.settings.impl.SelectorSetting
 import com.odtheking.odin.features.Module
 import com.odtheking.odin.features.impl.skyblock.Splits.showTickTime
 import com.odtheking.odin.utils.Colors
 import com.odtheking.odin.utils.formatTime
+import com.odtheking.odin.utils.modMessage
 import com.odtheking.odin.utils.render.getStringWidth
 import com.odtheking.odin.utils.render.text
 import com.odtheking.odin.utils.skyblock.SplitsManager.currentSplits
@@ -16,7 +19,10 @@ object ExtraSplits : Module(
 	description = "Extra timers for Kuudra and Dungeons that base Odin doesn't have."
 ) {
 	private val fixedWidth by BooleanSetting("Fixed Width", true, desc = "Always use a fixed HUD width, right-aligning the times.")
-	private val totalTimeSplits by BooleanSetting("Total Run Time Splits", true, desc = "Show splits for the full run.")
+	private val totalTimeSplits by BooleanSetting("Total Run Time Splits", true, desc = "Shows a split timer for the full run time.")
+	private val timeLostToLag by BooleanSetting("Time Lost To Lag", true, desc = "Shows a split timer for how much run time is lost to lag.").withDependency { showTickTime }
+	private val sendTimeLost by SelectorSetting("Send Time Lost", "Local", listOf("None", "Local", "Party", "Both"), desc = "Sends to the chat the run time lost to lag.").withDependency { timeLostToLag && showTickTime }
+//	private val endOfRunRegex = Regex("^\\s*☠ Defeated (.+) in 0?([\\dhms ]+?)\\s*(\\(NEW RECORD!\\))?$"), "§1Total")
 	private val extraSplitsHud by HUD("Extra Splits HUD", "Shows the extra split timers.") { extraSplitsExample ->
 		val totalWidth = getStringWidth("Split 0: 0h 00m 00s" + if (showTickTime) " (0h 00m 00s)" else "") + 2
 
@@ -32,7 +38,7 @@ object ExtraSplits : Module(
 			}
             return@HUD totalWidth to 9 * 1
 		}
-		
+
 		val (times, tickTimes, current) = getAndUpdateSplitsTimes(currentSplits)
 		if (currentSplits.splits.isEmpty()) return@HUD 0 to 0
 		
@@ -47,6 +53,23 @@ object ExtraSplits : Module(
 
 			text(displayText, timeX, (currentSplits.splits.size - 1) * 1, Colors.WHITE)
 		}
+
+		if (timeLostToLag && totalTimeSplits && showTickTime) {
+			text("§c§lTime Lost", 0, (currentSplits.splits.size - 1) * 2, Colors.WHITE)
+
+			val totalTime = times.take(9).sum() / 1000f
+			val totalTickTime = tickTimes.take(9).sum() / 20f
+			val timeDifference = totalTime - totalTickTime
+			val displayText = timeDifference.toFixed()
+			val timeX = if (fixedWidth) totalWidth - getStringWidth(displayText) else maxWidth + 4
+
+			text(displayText, timeX, (currentSplits.splits.size - 1) * 2, Colors.WHITE)
+
+			if (sendTimeLost == 1 && timeLostToLag) {
+				modMessage("$displayText lost to lag.")
+			}
+		}
+
 		totalWidth to 9 * (currentSplits.splits.size + 0)
 	}
 }
