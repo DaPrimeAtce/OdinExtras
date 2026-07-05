2a2a83efefdb45be2bda269c307cf866fe353a34
DaPrimeAtce <daprimeatce@gmail.com>
2026-07-04 20:16:21 -0600

    (WIP) Add ExtraSplits
    - pretty much the same as base Odin with a few changes, could use some cleanup

---
+++ src/main/kotlin/com/daprimeatce/odinextras/features/impl/skyblock/ExtraSplits.kt
@@ -0,0 +1,52 @@
+package com.daprimeatce.odinextras.features.impl.skyblock
+
+import com.odtheking.odin.clickgui.settings.impl.BooleanSetting
+import com.odtheking.odin.features.Module
+import com.odtheking.odin.features.impl.skyblock.Splits.showTickTime
+import com.odtheking.odin.utils.Colors
+import com.odtheking.odin.utils.formatTime
+import com.odtheking.odin.utils.render.getStringWidth
+import com.odtheking.odin.utils.render.text
+import com.odtheking.odin.utils.skyblock.SplitsManager.currentSplits
+import com.odtheking.odin.utils.skyblock.SplitsManager.getAndUpdateSplitsTimes
+import com.odtheking.odin.utils.toFixed
+
+object ExtraSplits : Module(
+	name = "Extra Splits",
+	description = "Extra timers for Kuudra and Dungeons that base Odin doesn't have."
+) {
+	private val fixedWidth by BooleanSetting("Fixed Width", true, desc = "Always use a fixed HUD width, right-aligning the times.")
+	private val totalTimeSplits by BooleanSetting("Total Run Time Splits", true, desc = "Show splits for the full run.")
+	private val extraSplitsHud by HUD("Extra Splits HUD", "Shows the extra split timers.") { extraSplitsExample ->
+		val totalWidth = getStringWidth("Split 0: 0h 00m 00s" + if (showTickTime) " (0h 00m 00s)" else "") + 2
+
+		if (extraSplitsExample) {
+			repeat(1) { i ->
+				val exampleTime = "0h 00m 00.00s" + if (showTickTime) " §8(§70s§8)" else ""
+				if (fixedWidth) {
+					text("§a§lRun Time:", 0, i * 9, Colors.WHITE)
+					text(exampleTime, totalWidth - getStringWidth("0h 00m 00.00s" + if (showTickTime) " (0s)" else ""), i * 9, Colors.WHITE)
+				} else {
+					text("§a§lRun Time: §r$exampleTime", 0, i * 9, Colors.WHITE)
+				}
+			}
+            return@HUD totalWidth to 9 * 1
+		}
+		
+		val (times, tickTimes, current) = getAndUpdateSplitsTimes(currentSplits)
+		if (currentSplits.splits.isEmpty()) return@HUD 0 to 0
+		
+		val maxWidth = currentSplits.splits.dropLast(1).maxOf { getStringWidth(it.name) }
+		
+		if (totalTimeSplits && currentSplits.splits.size > 9) {
+			text("§a§lRun Time", 0, (currentSplits.splits.size - 1) * 1, Colors.WHITE)
+
+			val totalTime = formatTime(times.take(9).sum())
+			val displayText = if (showTickTime) "$totalTime §8(§7${(tickTimes.take(9).sum() / 20f).toFixed()}§8)" else totalTime
+			val timeX = if (fixedWidth) totalWidth - getStringWidth(displayText) else maxWidth + 4
+
+			text(displayText, timeX, (currentSplits.splits.size - 1) * 1, Colors.WHITE)
+		}
+		totalWidth to 9 * (currentSplits.splits.size + 0)
+	}
+}
