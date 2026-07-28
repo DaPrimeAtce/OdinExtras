01915a772d89b7287f4c1f72c6157073ce0c576f
User <113803739+TurtleGD@users.noreply.github.com>
2026-07-28 04:12:16 -0400

    Added GUI Scale Customization
    Added to Click GUI+ module with a range between 0.5-1.5

--- src/main/kotlin/com/daprimeatce/odinextras/features/impl/render/ClickGUIPlus.kt
+++ src/main/kotlin/com/daprimeatce/odinextras/features/impl/render/ClickGUIPlus.kt
@@ -1,6 +1,7 @@
 package com.daprimeatce.odinextras.features.impl.render
 
 import com.odtheking.odin.clickgui.settings.impl.BooleanSetting
+import com.odtheking.odin.clickgui.settings.impl.StringSetting
 import com.odtheking.odin.features.Module
 
 object ClickGUIPlus : Module(
@@ -8,10 +9,5 @@ object ClickGUIPlus : Module(
     description = "Extra options for Odin's Click GUI."
 ) {
     val alphabeticalSorting by BooleanSetting("Alphabetical Sorting", true, desc = "Sorts the module lists alphabetically instead of Odin's default widest-first order.")
-
-    fun azSorting(): Boolean? {
-        if (!enabled || !alphabeticalSorting) return null
-        return alphabeticalSorting
-    }
-
+    val scale by StringSetting("GUI Scale", "1", desc = "Enables a custom GUI scale between 0.5 and 1.5.")
 }
