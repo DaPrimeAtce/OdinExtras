c9c6e432a5425e2ee711d0f915ae9cfed1d61609
User <113803739+TurtleGD@users.noreply.github.com>
2026-07-28 04:23:33 -0400

    Fixed GUI Scale Customization
    I FORGOT TO PUSH THE MIXIN

---
+++ src/main/java/com/daprimeatce/odinextras/mixin/MixinClickGUIModule.java
@@ -0,0 +1,24 @@
+package com.daprimeatce.odinextras.mixin;
+
+import com.daprimeatce.odinextras.features.impl.render.ClickGUIPlus;
+import com.odtheking.odin.features.impl.render.ClickGUIModule;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = ClickGUIModule.class, remap = false)
+public class MixinClickGUIModule {
+    @Inject(method = "getStandardGuiScale", at = @At("HEAD"), cancellable = true)
+    private void getStandardGuiScale(CallbackInfoReturnable<Float> cir) {
+        if (ClickGUIPlus.INSTANCE.getEnabled()) {
+            try {
+                cir.setReturnValue(Math.clamp(Float.parseFloat(ClickGUIPlus.INSTANCE.getScale()), 0.5f, 1.5f));
+                cir.cancel();
+            } catch (Exception _) {
+                cir.setReturnValue(1f);
+                cir.cancel();
+            }
+        }
+    }
+}
