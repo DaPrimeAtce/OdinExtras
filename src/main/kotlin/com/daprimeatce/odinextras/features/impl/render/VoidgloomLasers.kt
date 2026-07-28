a5b358fafba1e37625a56ee4b8c141f3c9031c27
User <113803739+TurtleGD@users.noreply.github.com>
2026-07-28 00:18:32 -0400

    Voidgloom Lasers rendering fix
    Changed a return statement so multiple timers can now render, may clean up logic sometime but no idea what to change

--- src/main/kotlin/com/daprimeatce/odinextras/features/impl/render/VoidgloomLasers.kt
+++ src/main/kotlin/com/daprimeatce/odinextras/features/impl/render/VoidgloomLasers.kt
@@ -56,8 +56,8 @@ object VoidgloomLasers : Module(
 
         on<RenderEvent.Extract> {
             endermen.forEach { (eman, ticks) ->
-                if (ticks < 0) return@on
-                val emanEntity = eman.asLivingEntity() ?: return@on
+                if (ticks < 0) return@forEach
+                val emanEntity = eman.asLivingEntity() ?: return@forEach
 
                 // Make sure this isn't basically esp
                 val camera = mc.gameRenderer.mainCamera.blockPosition()
