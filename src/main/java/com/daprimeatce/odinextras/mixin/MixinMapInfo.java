5cb7ed6d2539120c63112ded812b9a407adab892
User <113803739+TurtleGD@users.noreply.github.com>
2026-08-02 18:58:29 -0400

    Added custom 300 score to base Map Info module

---
+++ src/main/java/com/daprimeatce/odinextras/mixin/MixinMapInfo.java
@@ -0,0 +1,94 @@
+package com.daprimeatce.odinextras.mixin;
+
+import com.daprimeatce.odinextras.utils.SharedMixinState;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.odtheking.odin.clickgui.settings.Setting;
+import com.odtheking.odin.clickgui.settings.impl.BooleanSetting;
+import com.odtheking.odin.clickgui.settings.impl.DropdownSetting;
+import com.odtheking.odin.clickgui.settings.impl.StringSetting;
+import com.odtheking.odin.features.impl.dungeon.MapInfo;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import java.util.LinkedHashMap;
+
+@Mixin(MapInfo.class)
+@SuppressWarnings("unused")
+abstract class MixinMapInfo {
+    @Unique
+    private static DropdownSetting odinextras$score300Dropdown;
+    @Unique
+    private static StringSetting odinextras$score300CustomTitle;
+
+    @Inject(method = "<init>", at = @At("TAIL"))
+    private void odinextras$add300ScoreDropdownSetting(CallbackInfo ci) {
+        odinextras$score300Dropdown = new DropdownSetting(
+                "300 Score",
+                false,
+                "Customize what happens when reaching 300 score."
+        );
+        SharedMixinState.score300Dropdown = odinextras$score300Dropdown;
+    }
+
+    @Inject(method = "<init>", at = @At("TAIL"))
+    private void odinextras$add300ScoreCustomTitleStringSetting(CallbackInfo ci) {
+        odinextras$score300CustomTitle = new StringSetting(
+                "Custom Title",
+                "&c300 Score",
+                64,
+                "What to render when reaching 300 score. Use \"&\" for color codes."
+        );
+        SharedMixinState.score300CustomTitle = odinextras$score300CustomTitle;
+        Setting.Companion.withDependency(odinextras$score300CustomTitle, () -> odinextras$score300Dropdown.getValue());
+    }
+
+    @Inject(method = "<clinit>", at = @At("TAIL"))
+    private static void odinextras$reorderScoreTitle(CallbackInfo ci) {
+        LinkedHashMap<String, Setting<?>> settings = MapInfo.INSTANCE.getSettings();
+
+        Setting<?> scoreTitle = settings.remove("300 Score Title");
+        if (scoreTitle == null) return;
+
+        // Might as well just register here for ordering
+        LinkedHashMap<String, Setting<?>> reordered = new LinkedHashMap<>();
+        reordered.put("300 Score", odinextras$score300Dropdown);
+        reordered.put("300 Score Title", scoreTitle);
+        reordered.put("Custom Title", odinextras$score300CustomTitle);
+        reordered.put("Print Score Time", odinextras$score300CustomTitle);
+        reordered.putAll(settings);
+
+        settings.clear();
+        settings.putAll(reordered);
+    }
+
+    @ModifyExpressionValue(method = "<clinit>", at = @At(value = "NEW", target = "Lcom/odtheking/odin/clickgui/settings/impl/BooleanSetting;", ordinal = 2))
+    private static BooleanSetting odinextras$addBase300ScoreTitleDependency(BooleanSetting original) {
+        return Setting.Companion.withDependency(original, () -> odinextras$score300Dropdown.getValue());
+    }
+
+    @ModifyExpressionValue(method = "<clinit>", at = @At(value = "NEW", target = "Lcom/odtheking/odin/clickgui/settings/impl/BooleanSetting;", ordinal = 3))
+    private static BooleanSetting odinextras$addBase300ScorePrintWhenScoreDependency(BooleanSetting original) {
+        return Setting.Companion.withDependency(original, () -> odinextras$score300Dropdown.getValue());
+    }
+}
+
+
+
+
+@Mixin(targets = "com.odtheking.odin.features.impl.dungeon.MapInfo$2", remap = false)
+@SuppressWarnings("unused")
+abstract class MixinMapInfo$2 {
+    @SuppressWarnings("UnresolvedMixinReference")
+    @ModifyArg(
+            method = "invoke()V",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lcom/odtheking/odin/utils/PlayerUtilsKt;alert$default(Ljava/lang/String;ZILjava/lang/Object;)V"
+            ),
+            index = 0
+    )
+    private static String odinextras$modifyAlertMessage(String original) {
+        return SharedMixinState.score300CustomTitle.getValue().replace("&", "§");
+    }
+}
---
+++ src/main/java/com/daprimeatce/odinextras/utils/SharedMixinState.java
@@ -0,0 +1,9 @@
+package com.daprimeatce.odinextras.utils;
+
+import com.odtheking.odin.clickgui.settings.impl.DropdownSetting;
+import com.odtheking.odin.clickgui.settings.impl.StringSetting;
+
+public class SharedMixinState {
+    public static DropdownSetting score300Dropdown;
+    public static StringSetting score300CustomTitle;
+}
