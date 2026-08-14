8a5587c77a9dd8f1c741aeb282275f84401418a4
User <113803739+TurtleGD@users.noreply.github.com>
2026-08-14 16:40:45 -0400

    Reorganized TeammateDeaths and added a way to use ign for death message

--- src/main/kotlin/com/daprimeatce/odinextras/features/impl/dungeon/TeammateDeaths.kt
+++ src/main/kotlin/com/daprimeatce/odinextras/features/impl/dungeon/TeammateDeaths.kt
@@ -5,51 +5,45 @@ import com.odtheking.odin.clickgui.settings.impl.BooleanSetting
 import com.odtheking.odin.clickgui.settings.impl.NumberSetting
 import com.odtheking.odin.clickgui.settings.impl.StringSetting
 import com.odtheking.odin.features.Module
-import com.odtheking.odin.utils.handlers.schedule
 import com.odtheking.odin.utils.sendCommand
 import com.odtheking.odin.utils.alert
 import com.odtheking.odin.events.ChatPacketEvent
 import com.odtheking.odin.events.LevelEvent
 import com.odtheking.odin.events.core.on
 import com.daprimeatce.odinextras.utils.RegexUtils
+import com.odtheking.odin.clickgui.settings.impl.DropdownSetting
+import com.odtheking.odin.utils.skyblock.dungeon.DungeonUtils
 
 object TeammateDeaths: Module(
     name = "Teammate Deaths",
     description = "Various features based on teammate deaths in a dungeon."
 ) {
-    private val deathAlert by BooleanSetting("Alerts", false, desc = "Sends an alert when your team reaches a threshold of deaths in a run.")
-    private val threshold by NumberSetting("Alert Threshold", 5f, 0f, 10f, 1f, unit = " deaths", desc = "The amount of deaths before the alert is sent.").withDependency { deathAlert }
-    private val displayTitle by BooleanSetting("Alert Title", false, desc = "Displays a title when the threshold of deaths in a run has been reached.").withDependency { deathAlert }
+    private val deathAlert by DropdownSetting("Alerts", false, desc = "Sends an alert when your team reaches a threshold of deaths in a run.")
+    private val threshold by NumberSetting("Alert Threshold", 5f, 0f, 10f, 1f, unit = " deaths", desc = "The number of deaths required for alerts.").withDependency { deathAlert }
+    private val displayTitle by BooleanSetting("Title On Threshold", false, desc = "Displays a title when the threshold of deaths in a run has been reached.").withDependency { deathAlert }
     private val titleText by StringSetting("Title", "&c&lS+ Lost!", desc = "What the title should display.").withDependency { deathAlert && displayTitle }
-    private val thresholdMessage by StringSetting("Message on Threshold", "{deaths} Deaths Reached!", desc = "The message to send in party chat when the teammate death threshold has been reached.").withDependency { deathAlert }
-    private val message by StringSetting("Message On Death", "", desc = "The message to send in party chat when a teammate dies in a run.")
+    private val sendThresholdMessage by BooleanSetting("Message On Threshold", false, desc = "Displays a title when the threshold of deaths in a run has been reached.").withDependency { deathAlert }
+    private val thresholdMessage by StringSetting("Message", "{deaths} Deaths Reached!", desc = "The message to send in party chat when the teammate death threshold has been reached. Use \"{deaths}\" for the number of deaths.").withDependency { deathAlert && sendThresholdMessage }
+    private val deathMessage by StringSetting("Death Message", "", desc = "The message to send in party chat when a teammate dies in a run. Use \"{name}\" for the player name.")
 
-    var deaths = 0
     var thresholdReached = false
 
     init {
         on<ChatPacketEvent> {
             if (RegexUtils.playerDeathRegex.matches(value)) {
-                deaths++
-                if (message.isNotEmpty()) {
-                    schedule(1, true) {
-                        sendCommand("pc $message")
-                    }
-                }
-                if (deaths == threshold.toInt() && !thresholdReached) {
-                    schedule(1, true) {
-                        sendCommand("pc " + thresholdMessage.replace("{deaths}", "$deaths"))
-                        if (displayTitle) {
-                            alert(titleText.replace("&", "§"))
-                        }
-                    }
+                var ign = RegexUtils.playerDeathRegex.find(value)?.groupValues[2]
+                if (ign != null && ign == "You") ign = mc.player?.name?.string
+                if (DungeonUtils.deathCount + 1 == threshold.toInt() && !thresholdReached) { // Should work assuming that tab list always updates after the chat message (which it generally should)
+                    if (sendThresholdMessage) sendCommand("pc " + thresholdMessage.replace("{deaths}", "${DungeonUtils.deathCount + 1}"))
+                    if (displayTitle) alert(titleText.replace("&", "§"))
                     thresholdReached = true
                 }
+                if (deathMessage.isNotEmpty() && ign != null) sendCommand("pc " + deathMessage.replace("{name}", ign))
             }
         }
+
         on<LevelEvent.Load> {
             thresholdReached = false
-            deaths = 0
         }
     }
 }
